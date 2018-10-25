49f51906eeca99fc2301c5c183c8abff307698b1
shadish <shadtisch@gmail.com>
2018-10-25 00:02:36 -0600

    add serve.js

---
+++ serve.js
@@ -0,0 +1,12 @@
+const redisClient = require('./redis-client');
+app.get('/store/:key', async (req, res) => {
+    const { key } = req.params;
+    const value = req.query;
+    await redisClient.setAsync(key, JSON.stringify(value));
+    return res.send('Success');
+});
+app.get('/:key', async (req, res) => {
+    const { key } = req.params;
+    const rawData = await redisClient.getAsync(key);
+    return res.json(JSON.parse(rawData));
+});
