1cb5e3de40da89d8223713b2d83845273be74fa8
shadish <shadtisch@gmail.com>
2018-10-25 00:00:16 -0600

    add redis to package and create client host

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
+const redis = require('redis');
+const {promisify} = require('util');
+const client = redis.createClient(process.env.REDIS_URL);
 
-app.get('/', (req, res) => {
-    return res.send('Hello world');
-});
-
-app.listen(3000, () => {
-    console.log('hosting on 3000');
-});
+module.exports = {
+  ...client,
+  getAsync: promisify(client.get).bind(client),
+  setAsync: promisify(client.set).bind(client),
+  keysAsync: promisify(client.keys).bind(client)
+};
